b0590f5ee6e0f049982f889751a3d1b08c7628a4
1DiEgo11 <sona.egor23@gmail.com>
2023-03-20 20:07:09 +1000

    Добавьте файлы проекта.

---
+++ Character/Character.cs
@@ -0,0 +1,60 @@
+﻿namespace character
+{
+
+    public class Person
+    {
+        public int type_of_person;
+        public int hurt;
+        public int[] coordinates;
+        public int money;
+        public string[]? inventory;
+        public int mana;
+        public string weapon_type;
+        public double damage;
+        public int armor;
+        public int level;
+
+
+
+        public Person()
+        {
+            this.type_of_person = 0;
+            this.hurt = 6;
+            this.money = 0;
+            this.mana = 20;
+            this.damage = 2;
+            this.armor = 0;
+            this.level = 0;
+        }
+    }
+
+    public class Warrior : Person
+    {
+        public Warrior(int[] coordinates)
+        {
+            this.type_of_person = 1;
+            this.coordinates = coordinates;
+            this.armor++;
+        }
+    }
+
+    public class Archer : Person
+    {
+        public Archer(int[] coordinates)
+        {
+            this.type_of_person = 2;
+            this.coordinates = coordinates;
+        }
+    }
+
+    public class Mage : Person
+    {
+        public Mage(int[] coordinates)
+        {
+            this.type_of_person = 3;
+            this.coordinates = coordinates;
+            this.mana = 100;
+        }
+    }
+
+}
---
+++ Game(console)/Program.cs
@@ -0,0 +1,54 @@
+﻿using character;
+
+int[] coordinates = { 15, 15 };
+
+var person = new Warrior(coordinates);
+
+
+Console.CursorVisible = false;
+Console.ForegroundColor = ConsoleColor.Green;
+ConsoleKeyInfo keyInfo;
+
+char playerChar = 'X';
+
+// Координаты флага
+int flagX = 25;
+int flagY = 5;
+
+do
+{
+    Console.Clear();
+    Console.SetCursorPosition(flagX, flagY);
+    Console.Write("F");
+
+    Console.SetCursorPosition(person.coordinates[0], person.coordinates[1]);
+    Console.Write(playerChar);
+
+    keyInfo = Console.ReadKey();
+
+    playerChar = playerChar == 'X' ? 'Y' : 'X';
+
+    switch (keyInfo.Key)
+    {
+        case ConsoleKey.A:
+            person.coordinates[0]--;
+            break;
+        case ConsoleKey.D:
+            person.coordinates[0]++;
+            break;
+        case ConsoleKey.W:
+            person.coordinates[1]--;
+            break;
+        case ConsoleKey.S:
+            person.coordinates[1]++;
+            break;
+    }
+
+    if (person.coordinates[0] == flagX && person.coordinates[1] == flagY)
+    {
+        Console.Clear();
+        Console.WriteLine("EPIC WIN!");
+        break;
+    }
+
+} while (keyInfo.Key != ConsoleKey.Escape);
