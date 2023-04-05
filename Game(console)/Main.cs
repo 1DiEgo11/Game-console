6f99c130b264fdc2261fdd62f7a12b88d6aa1e1b
Tessia <kira.eberzin@mail.ru>
2023-04-06 09:19:30 +1000

    meow

--- Game(console)/Main.cs
+++ Game(console)/Main.cs
@@ -6,23 +6,6 @@ using weapon;
 using static Animation_Enemys.Animation;
 using Animation_Enemys;
 
-//Person player = new Person();
-//Chekanov_version2 enemy = new Chekanov_version2(1);
-//Rainbow_sword weapon = new Rainbow_sword();
-//Console.WriteLine(enemy.hp);
-//Battle.Battle.Attack(player, enemy, weapon);
-//Console.WriteLine(enemy.hp);
-//do
-////{
-//Chekanov bos = new Chekanov(1);
-//bos.draw(1, 1);
-//    ConsoleKeyInfo key = Console.ReadKey();
-//    Battle.Battle.Enemy_do(player, enemy, weapon, key);
-//    Console.WriteLine(player.heart);
-//    Console.WriteLine(enemy.hp);
-//} while (player.heart != 0 || enemy.hp != 0);
-//AnimationEnemy3();
-//Animation
 MovePerson.Game();
 Console.Clear();
 Console.WriteLine("До скорой встречи!!!");
