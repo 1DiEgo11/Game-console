bd25430100b68e5d1eea8c342944c9e7967bc09d
Tessia <kira.eberzin@mail.ru>
2023-04-06 01:37:01 +1000

    ааааааааа

--- Battle/Battle.cs
+++ Battle/Battle.cs
@@ -1,69 +1,77 @@
-﻿//using character;
-//using enemy;
-//using weapon;
+﻿using character;
+using enemy;
+using weapon;
 
 namespace Battle
 {
-    //    public class Battle
-    //    {
+    public class Battle
+    {
+        ConsoleKeyInfo keyInfo = Console.ReadKey();
+        public static void Attack(Person player, Enemy enemy, Weapon weapon)
+        {
+            enemy.hp -= player.damage * weapon.Damage;
+            if (weapon.Type == "magic")
+            {
+                player.mana -= 5;
+            }
+        }
 
-    //        public static void Attack(Person player, Enemy enemy, Weapon weapon)
-    //        {
-    //            enemy.hp -= player.damage * weapon.Damage;
-    //            if (weapon.Type == "magic")
-    //            {
-    //                player.mana -= 5;
-    //            }
-    //        }
-
-    //        public static void Miss(Person player, Enemy enemy, Weapon deffense)
-    //        {
-    //            Random random = new Random();
-    //            int res = random.Next(0, 11);
-    //            if (res < 6)
-    //            {
-    //                player.heart -= enemy.damage * deffense.Damage;
-    //            }
-    //        }
-    //        public static void Stand(Person player, Enemy enemy, Weapon deffense)
-    //        {
-    //            player.heart -= enemy.damage * deffense.Damage;
-    //        }
+        public static void Miss(Person player, Enemy enemy, Weapon deffense)
+        {
+            Random random = new Random();
+            int res = random.Next(0, 11);
+            if (res < 6)
+            {
+                player.heart -= enemy.damage * deffense.Damage;
+            }
+        }
+        public static void Stand(Person player, Enemy enemy, Weapon deffense)
+        {
+            player.heart -= enemy.damage * deffense.Damage;
+        }
 
 
-    //        public static void Enemy_miss(Person player, Enemy enemy, Weapon weapon)
-    //        {
-    //            Random random = new Random();
-    //            int res = random.Next(0, 11);
-    //            if (res < 6)
-    //            {
-    //                Battle.Attack(player, enemy, weapon);
-    //            }
-    //        }
+        public static void Enemy_miss(Person player, Enemy enemy, Weapon weapon)
+        {
+            Random random = new Random();
+            int res = random.Next(0, 11);
+            if (res < 6)
+            {
+                Battle.Attack(player, enemy, weapon);
+            }
+        }
 
-    //        public static void Enemy_do(Person player, Enemy enemy, Weapon weapon)
-    //        {
-    //            Random random = new Random();
-    //            int res = random.Next(0, 11);
-    //            if (res < 6)
-    //            {
-    //                if (key.Key == ConsoleKey.X)
-    //                    Battle.Miss(player, enemy, weapon);
+        public static void Enemy_do(Person player, Enemy enemy, Weapon weapon, ConsoleKeyInfo keyInfo)
+        {
+            Random random = new Random();
+            int res = random.Next(0, 11);
+            Console.WriteLine(res);
+            if (res < 6) //attack
+            {
+                if (keyInfo.Key == ConsoleKey.X)
+                {
+                    Battle.Miss(player, enemy, weapon);
+                }
+                else 
+                {
+                    if (keyInfo.Key == ConsoleKey.Z)
+                    {
+                        //Battle.Attack(player, enemy, weapon);
+                        enemy.hp -= player.damage * weapon.Damage;
+                    }
+                        player.heart -= enemy.damage * weapon.Damage;
+                }
+            }
+            else
+            {
+                if (keyInfo.Key == ConsoleKey.Z)
+                {
+                    Battle.Enemy_miss(player, enemy, weapon);
+                }
+            }
 
-    //            }
-    //            else
-    //            {
-    //                if (key.key == ConsoleKey.Z)
-    //                {
-    //                    Battle.Enemy_miss(player, enemy, weapon);
-    //                }
-    //            }
+        }
+        
+    }
 
-    //        }
-    //        //do 
-    //        //{
-    //        //    ConsoleKeyInfo key = Console.ReadKey();
-    //        //    Battle.Battle.Enemy_do
-    //        //} while (player.heart!=0 || enemy.hp!=0)
-    //    }
 }
